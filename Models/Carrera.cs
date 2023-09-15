422267b2f6ade8db2ea4c057cb3311ed3dd01a39
siriuzz <elianmatosd03+github@gmail.com>
2023-09-15 13:14:10 -0400

    modelo carrera y modelo estudiante creados

---
+++ Models/Carrera.cs
@@ -0,0 +1,10 @@
+﻿namespace IndiC_API.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Periodos_Totales { get; set; }
+        public int Asignaturas_Totales { get; set; }
+    }
+}
---
+++ Models/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace IndiC_API.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono {  get; set; }
+        public string Cedula { get; set; }
+        public string Password { get; set; }
+        public DateTime Fecha_Registro { get; set; }
+        public string Direccion {  get; set; }
+        public string Configuracion {  get; set; }
+        //public int Id_Carrera { get; set; }
+        //public int Id_Asignatura { get;set; }
+        //public int Id_Estado { get; set; }
+        //public int Id_Rol { get; set; }
+    }
+}
